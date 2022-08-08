27112bff114b85e3627ff8c6e12ed41bbe681d3f
ElenaG <freshelf@yandex.ru>
2022-08-08 23:39:21 +0500

    add Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,8 @@
+﻿// проверить 2 числа, является ли одно из них квадратом другого.
+
+Console.Write("a = ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2 || number2 == number1 * number1) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
