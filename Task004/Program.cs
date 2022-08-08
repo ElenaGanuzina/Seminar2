af351a339d8e70d9e198edc4d96d4f796c5f586d
ElenaG <freshelf@yandex.ru>
2022-08-08 23:25:44 +0500

    add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿// вход число, кратно ли оно и 7, и 23?
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
